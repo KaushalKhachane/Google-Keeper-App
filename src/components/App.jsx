6ba43658ed4c936e62fa7f652f654372ba11932d
Kaushal Khachane <khachaneks22@gmail.com>
2023-08-06 07:50:25 +0530

    Adding CreateArea.jsx file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,24 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes.js";
+import CreateArea from "./CreateArea";
 
 function createNotes(noteItem) {
   return <Note title={noteItem.title} content={noteItem.content} />;
 }
 
 function App() {
+
+  function addNote(note){
+    console.log(note);
+  }
+
   return (
     <div>
       <Header />
+      <CreateArea 
+        onAdd={addNote}
+      />
       {notes.map(createNotes)}
       <Footer />
     </div>
